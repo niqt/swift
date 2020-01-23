0be227e7ea7df1e3aed4078af237608447b9ea0c
Nicola De Filippo <nicola@nicoladefilippo.it>
2020-01-23 10:01:43 +0100

    First card

---
+++ Card/Card/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Card
+//
+//  Created by Nicola De FIlippo on 23/01/2020.
+//  Copyright © 2020 Nicola De FIlippo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            card(imageName: "sanfrancisco", text: "Bay")
+            card(imageName: "giulianova", text: "Sea")
+            card(imageName: "berkeley", text: "Library")
+            card(imageName: "stanford", text: "Church")
+            //Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct card: View {
+    var imageName: String
+    var text: String
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            //.edgesIgnoringSafeArea(.all)
+            .aspectRatio(contentMode: .fit)
+            //.aspectRatio(contentMode: .fill)
+            .frame(width: 300)
+            //.clipShape(Circle())
+            .opacity(0.5)
+            .overlay(
+                Text(text)
+                    .fontWeight(.heavy)
+                    .font(.system(.headline, design: .rounded))
+                    .foregroundColor(.yellow)
+                    .padding()
+                    .background(Color.gray)
+                    .cornerRadius(20)
+                    .opacity(0.8)
+                    .padding(),
+                alignment: .top
+        )
+    }
+}

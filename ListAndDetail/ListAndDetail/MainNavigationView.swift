fc4605a4100b5352d78c4f399e2bc2960242c202
Nicola De Filippo <nicola@nicoladefilippo.it>
2020-02-02 20:12:51 +0100

    Add navlink in topbar

--- ListAndDetail/ListAndDetail/MainNavigationView.swift
+++ ListAndDetail/ListAndDetail/MainNavigationView.swift
@@ -10,28 +10,48 @@ import SwiftUI
 
 struct MainNavigationView: View {
     var places = [Place(name: "San Francisco", image: "sanfrancisco"),
-    Place(name: "Stanford", image: "stanford"),
-    Place(name: "Berkeley", image: "berkeley"),
-    Place(name: "Giulianova", image: "giulianova"),
-    Place(name: "Berlin", image: "berlin"),
-    Place(name: "Benevento", image: "bn")]
-  
+                  Place(name: "Stanford", image: "stanford"),
+                  Place(name: "Berkeley", image: "berkeley"),
+                  Place(name: "Giulianova", image: "giulianova"),
+                  Place(name: "Berlin", image: "berlin"),
+                  Place(name: "Benevento", image: "bn")]
+    
+    @State private var searchTerm : String = ""
+    
     var body: some View {
         
         NavigationView {
-
-            List {
-                ForEach(places) { place in
-                    NavigationLink(destination: PlaceDetailView(place: place)) {
-                        BasicImageRow(place: place)
+            
+            VStack {
+                SearchView(text: $searchTerm)
+                List {
+                    
+                    /*ForEach(places) { place in
+                     NavigationLink(destination: PlaceDetailView(place: place)) {
+                     BasicImageRow(place: place)
+                     }
+                     
+                     }*/
+                    ForEach(self.places.filter {
+                        self.searchTerm.isEmpty ? true : $0.name.localizedStandardContains(self.searchTerm)
+                    }) { place in
+                        NavigationLink(destination: PlaceDetailView(place: place)) {
+                            BasicImageRow(place: place)
+                        }
                     }
-                
+                    
                 }
+                    //.navigationBarTitle("Places")
+                    .navigationBarTitle("Places", displayMode: .inline)
                 
+                //.listStyle(GroupedListStyle())
             }
-            //.navigationBarTitle("Places")
-            .navigationBarTitle("Places", displayMode: .inline)
-            .listStyle(GroupedListStyle())
+            .navigationBarItems(trailing:
+                
+                NavigationLink(destination: PlaceDetailView(place: Place(name: "", image: ""))) {
+                    Image(systemName: "plus.app")
+                }
+            )
         }
     }
 }

1d5ed35e4c1c6dc4aa3f698e8845e8ea8cf0caeb
niqt <nicola@nicoladefilippo.it>
2023-12-17 17:30:15 +0100

    Commit tree

---
+++ ChristmasTree/ChristmasTree/ChristmasTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasTreeApp.swift
+//  ChristmasTree
+//
+//  Created by nicola de filippo on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChristmasTree/ChristmasTree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChristmasTree
+//
+//  Created by nicola de filippo on 17/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
